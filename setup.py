13284563fed0868349d9835be8266151de55c658
Ian Davis <Ian.Davis@hobartcorp.com>
2016-04-28 15:15:27 -0400

    Adding setup script for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_utilities',
+    version='1.0.0',
+    packages=('python_utils', 'python_utils.os_util'),
+    url = 'https://github.com/TheKewlStore/PythonUtilities',
+    license = '',
+    author = 'Ian Davis',
+    author_email = 'thekewlstore@gmail.com',
+    description = "A set of simple utility APIs for higher-level python functions.",
+    )
